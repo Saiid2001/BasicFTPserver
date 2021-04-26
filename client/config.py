66698c4237a4401682899b822ba8cf838511507e
Saiid <saidhajjchehade@gmail.com>
2021-04-26 12:48:31 +0300

    fixed directory bug

--- client/config.py
+++ client/config.py
@@ -1,6 +1,6 @@
 import os
 #config
-ROOT_PATH = os.getcwd()
+ROOT_PATH = os.path.dirname(__file__)
 FILE_PATH = ROOT_PATH+"\\files"
 LIST_PATH = ROOT_PATH+"\\files.json"
 SERVER_IP = '127.0.0.1'
--- server/config.py
+++ server/config.py
@@ -1,6 +1,6 @@
 import os
 #config
-ROOT_PATH = os.getcwd()
+ROOT_PATH = os.path.dirname(__file__)
 FILE_PATH = ROOT_PATH+"\\files"
 LIST_PATH = ROOT_PATH+"\\files.json"
 HOST_IP = '0.0.0.0'
